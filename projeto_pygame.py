d574b73e2157a284ce525c6f220590e4e9839392
Licia Sales <40764768+liciascl@users.noreply.github.com>
2025-01-18 21:55:20 -0300

    Update projeto_pygame.py

--- projeto_pygame.py
+++ projeto_pygame.py
@@ -2,36 +2,34 @@ import pygame
 from funcoes import * 
 
 
-# Função principal do jogo
 def main():
-    # Chama a função que inicializa a grade 4x4 com blocos iniciais
-    grid = initialize_grid()
+     # Chama a função que inicializa a grade 4x4 com blocos iniciais
+    grade = inicializar_grade()
     # Define a variável de controle do loop principal do jogo
-    running = True
+    rodando = True
 
     # Loop principal do jogo
-    while running:
-        # Itera sobre todos os eventos que ocorrem no jogo (como teclas pressionadas ou o fechamento da janela)
-        for event in pygame.event.get():
+    while rodando:
+        # Captura todos os eventos que ocorrem no jogo (como teclas pressionadas ou o fechamento da janela)
+        for evento in pygame.event.get():
             # Verifica se o evento é fechar a janela do jogo
-            if event.type == pygame.QUIT:
+            if evento.type == pygame.QUIT:
                 # Interrompe o loop principal, fechando o jogo
-                running = False
+                rodando = False
             # Verifica se alguma tecla foi pressionada
-            elif event.type == pygame.KEYDOWN:
-                # Chama a função para capturar as entradas do teclado e processar os movimentos da grade
-                handle_input(grid, event)
+            elif evento.type == pygame.KEYDOWN:
+                # Chama a função para capturar as entradas do teclado 
+                capturar_entrada(grade, evento)
         
         # Renderiza a grade atualizada na tela
-        draw_grid(grid)
+        desenhar_grade(grade)
         # Atualiza a tela para exibir as mudanças
         pygame.display.flip()
         # Controla a velocidade do loop para rodar a 30 quadros por segundo
-        clock.tick(30)
+        relogio.tick(30)
     
     # Encerra o Pygame ao sair do loop
     pygame.quit()
 
 if __name__ == "__main__":
-    # Chama a função principal do jogo
     main()
